201a7bee488b7cce89993120ae8f74b8ede0af2c
Rajaruban Rajindram <ahyat888@gmail.com>
2023-02-28 21:11:03 +0800

    Preventing Unnecessary Re-Evaluations with React.memo()

--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -7,4 +7,7 @@ const DemoOutput = (props) => {
   return <Paragraph>{props.show ? "Its Showtime!!!" : "No Show!!!"}</Paragraph>;
 };
 
-export default DemoOutput;
+// React.memo() is a optimization method which takes a component as arguement and ensure to only to re-render the component when props of this component changes in value
+// even when the parent of this component re-renders. This method is only useful when have large sub-component tree, otherwise this method has its own performance costs
+// where React compares previous props value with new one on each re-render and stores a snapshot of previous props for re-rendering evaluation
+export default React.memo(DemoOutput);
